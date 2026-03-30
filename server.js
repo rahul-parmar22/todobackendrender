9b45e395914bed38c15458bbb347767577309b2a
Rahul Parmar <parmarnanubhaiv@gmail.com>
2026-03-30 17:57:34 +0530

    add methods in cors

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ dotenv.config();
 const port = process.env.PORT 
 
 app.use(cors({
-    origin:[ "https://meretodos.netlify.app","http://localhost:5173"],
-    credentials:true
-}
-))
+  origin: ["https://meretodos.netlify.app", "http://localhost:5173"],
+  credentials: true,
+  methods: ['GET','POST','PUT','DELETE']
+}));
 
 
 connectDB(); 
