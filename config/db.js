f9396130ac339c3456289e6b53c1393a22b6db17
Rahul Parmar <parmarnanubhaiv@gmail.com>
2026-03-30 12:57:40 +0530

    change mongouri

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/todoMyself");
+    await mongoose.connect(process.env.MONGO_ATLAS_URI);
     console.log("Mongodb connect successfully...!")
   } catch (err) {
     console.log(err);
