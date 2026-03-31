8c8c7364becfedb68a01d312de9b2c6b8bb1fd02
Rahul Parmar <parmarnanubhaiv@gmail.com>
2026-03-31 12:02:30 +0530

    update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ dotenv.config();
 const port = process.env.PORT 
 
 app.use(cors({
-  origin: ["https://meretodos.netlify.app", "http://localhost:5173"],
+  origin: "https://meretodos.netlify.app", 
   credentials: true,
   methods: ['GET','POST','PUT','DELETE']
 }));
